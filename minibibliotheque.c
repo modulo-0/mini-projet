8ba6c5e57a3d2cb665995763ba05b06e6027928b
modulo-0 <modulo0infinite@gmail.com>
2026-03-13 11:47:51 +0000

    la fonction d'ajouter à été corriger marche parfaitement

--- minibibliotheque.c
+++ minibibliotheque.c
@@ -13,7 +13,6 @@ typedef struct {
 
 void ajoutlivre(Livre bibliotheque[], int *nblivre) 
 {
-	int i = 0;
 	printf("Saisir le titre du livre:  \n");
 	gets(bibliotheque[*nblivre].nom);
 
@@ -30,6 +29,7 @@ void ajoutlivre(Livre bibliotheque[], int *nblivre)
 
 	printf("Saisir l'annee d'edition: ");
 	scanf("%d", &bibliotheque[*nblivre].annee);
+	getchar(); // pour consommer le caractère de nouvelle ligne après la saisie de l'année
 	
 	(*nblivre)++;
 }
@@ -39,6 +39,7 @@ int main()
 {
 	Livre bibliotheque[100];
 	int nblivre = 0;
+	int choix;
 	int i = 0;
 
 
@@ -48,21 +49,45 @@ int main()
 	puts("3. Rechercher un livre \n");
 	puts("4. Supprimer un livre \n");
 	puts("Votre choix \n");
-	
-	ajoutlivre(bibliotheque, &nblivre);
-	printf("%p\n", &nblivre);
 
-	ajoutlivre(bibliotheque, &nblivre);
-	printf("%d\n", &nblivre);
+	scanf("%d", &choix);
 
-	ajoutlivre(bibliotheque, &nblivre);
-	printf("%d\n", &nblivre);
+	switch (choix)
+	{
+	case 1:
+	{
+		int nb = 0;
+		puts("Entrez le nombre de livre à ajouter: \n");
+		scanf("%d", &nb);
+		for (int j = 0; j < nb; j++);
+		{
+			ajoutlivre(bibliotheque, &nblivre);
+		}
+		printf("%d\n", nblivre);
+		break;
+	}
 
+	case 2:
+	{
+		printf("Voici la liste des livres dans la bibliotheque: \n");
+		for (int i = 0; i < nblivre; i++);
+		{
+			printf("Titre: %s\n", bibliotheque[i].nom);
+			printf("Auteur: %s\n", bibliotheque[i].auteur);
+			printf("Annee d'edition: %d\n", bibliotheque[i].annee);
+			printf("\n");
+
+		}
+		break;
+	}
+	default:
+		printf("Choix invalide !!\n");
+	}
 	 
 	printf("Le nombre de livre dans la bibliotheque est de: %d\n", nblivre);
 	printf("\nVoici la liste des livres dans la bibliotheque: \n");
 
-	for (int i = 0; i < nblivre; i++);
+	for (int i = 0; i < nblivre; i++)
 	{
 		printf("Titre: %s\n", bibliotheque[i].nom);
 		printf("Auteur: %s\n", bibliotheque[i].auteur);
